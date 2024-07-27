956a51572627868e4ebff7aaa7aee28fdf2950d2
Shivanshu Mishra <112168836+Ryomensukuna2003@users.noreply.github.com>
2024-07-27 12:56:57 +0530

    Update Parse Table.cpp

--- Parse Table.cpp
+++ Parse Table.cpp
@@ -1,89 +1,89 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace std;
-
-#define vll vector<long long int>
-#define ll long long
-#define pb push_back
+using namespace  __gnu_pbds;
+using namespace  std;
+ 
+//                                「本 物 の 柔 術 を 見 せ て や る」
+ 
+#define vll      vector<long long int>
+#define ll       long long
+#define pb       push_back
 const ll M = 1e9 + 7;
 const ll N = 1e5 + 5;
 const ll inf = 2e18;
-#define all(x) (x).begin(), (x).end()
-#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define forin(x, y) for (auto i = 0; i < x; i++) { int x; cin >> x; y.pb(x); }
-#define forout(x) for (auto y : x) { cout << y << ' '; }
-#define value_sort(v) sort(all(v), [](auto &l, auto &r) { return l.second < r.second; });
-typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+const int maxN = 2e5+1;
+const int logN = 19;
+#define all(x)   (x).begin(), (x).end()
+#define FAST     ios_base ::sync_with_stdio(false); cin.tie(NULL)
+#define forin(x,y) for(auto i=0;i<x;i++){int x;cin>>x;y.pb(x);}
+#define forout(x)  for(auto y:x){cout<<y<<' ';}
+#define value_sort(v) sort(all(v),[](auto &l,auto &r){return l.second<r.second;});
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,tree_order_statistics_node_update> pbds;
 
-bool substringcheck(string s1, string s2) {
+bool substringcheck(string s1,string s2){
+    // String 1 in String 2
     if (s2.find(s1) != std::string::npos) return true;
     else return false;
 }
 
 void rem_dup_order(vector<ll> &vec1) {
-    auto it = unique(vec1.begin(), vec1.end());
+  auto it = unique(vec1.begin(), vec1.end());
     vec1.resize(distance(vec1.begin(), it));
 }
 
 ll mod(ll x) { return (x % M); }
-ll mod_minus(ll a, ll b) {
+ll mod_minus(ll a, ll b){
     ll ans = (mod(a) - mod(b));
     if (ans < 0) ans = mod(ans + M);
     return ans;
 }
 ll mod_mul(ll a, ll b) { return mod(mod(a) * mod(b)); }
 ll mod_add(ll a, ll b) { return mod(mod(a) + mod(b)); }
-ll power(ll a, ll n) {
+ll power(ll a, ll n){
     if (n == 0) return 1;
     else if (n == 1) return a;
     ll R = power(a, n / 2) % M;
     if (n % 2 == 0) return mod(mod_mul(R, R));
     else return mod(mod_mul(mod_mul(R, a), mod(R)));
 }
-ll mod_div(ll a, ll b) {
+ll mod_div(ll a, ll b){
     ll ans = mod(a);
     ll b1 = power(b, M - 2);
     ans = mod(mod_mul(ans, b1));
     return ans;
 }
 
-void solve() {
-    int K, x;
-    cin >> K >> x;
-    vector<vector<long long>> st(K + 1, vector<long long>(K));
-    vll vec1;
-    forin(K, vec1);
+int x[maxN][logN];
 
-    for (int j = 0; j < K; j++) {
-        st[0][j] = vec1[j];
-    }
+void solve()
+{
+    int N,Q,a,b,l,k;
+    cin>>N>>Q;
+    for(int i = 0; i < N; i++)
+        cin>>x[i][0];
 
-    for (int i = 1; i <= log2(K); i++) {
-        for (int j = 0; j + (1 << i) <= K; j++) {
-            st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]); // <-- Change the min to any function you like i.e., max,gcd,min
-        }
-    }
+    for(int i = 1; i < logN; i++)
+        for(int j = 0; j <= N-(1<<i); j++)
+            x[j][i] = min(x[j][i-1], x[j+(1<<(i-1))][i-1]);
 
-    for (int i = 0; i < x; i++) {
-        int l, r;
-        cin >> l >> r;
-        l--; r--; // Convert to 0-based index
-        int z = log2(r - l + 1);
-        int minimum = min(st[z][l], st[z][r - (1 << z) + 1]);
-        cout << minimum << endl;
+    for(int q = 0; q < Q; q++){
+        cin>>a>>b;
+        a--; b--;
+        l = b-a+1;
+        k = log2(l);
+        cout<<(min(x[a][k], x[b-(1<<k)+1][k]))<<endl;
     }
 }
 
-int main() {
+int main(){
     FAST;
-    ll t = 1;
-    //cin >> t;
-    while (t--) {
+    ll t=1;
+    //cin>>t;
+    while(t--){
         solve();
     }
-    return 0;
+return 0;
 }
 
 
