21aac87cf672b077dd980b549af2bc7678bf31e8
Shivanshu Mishra <112168836+Ryomensukuna2003@users.noreply.github.com>
2024-08-25 13:34:31 +0530

    Update Implementation.cpp

--- Implementation.cpp
+++ Implementation.cpp
@@ -1,7 +1,38 @@
+//                     「本 物 の 柔 術 を 見 せ て や る」
+
 #include <bits/stdc++.h>
 using namespace  std;
- 
-//                                「本 物 の 柔 術 を 見 せ て や る」
+
+// --------------------------------PBDS--------------------------------------------------
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace  __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+// --------------------------------PBDS--------------------------------------------------
+
+// --------------------------------Mod--------------------------------------------------
+ll mod(ll x) { return (x % M); }
+ll mod_minus(ll a, ll b){
+    ll ans = (mod(a) - mod(b));
+    if (ans < 0) ans = mod(ans + M);
+    return ans;
+}
+ll mod_mul(ll a, ll b) { return mod(mod(a) * mod(b)); }
+ll mod_add(ll a, ll b) { return mod(mod(a) + mod(b)); }
+ll power(ll a, ll n){
+    if (n == 0) return 1;
+    else if (n == 1) return a;
+    ll R = power(a, n / 2) % M;
+    if (n % 2 == 0) return mod(mod_mul(R, R));
+    else return mod(mod_mul(mod_mul(R, a), mod(R)));
+}
+ll mod_div(ll a, ll b){
+    ll ans = mod(a);
+    ll b1 = power(b, M - 2);
+    ans = mod(mod_mul(ans, b1));
+    return ans;
+}
+// --------------------------------Mod--------------------------------------------------
 
 
 // For checking String 1 in string 2
@@ -9,6 +40,12 @@ bool substringcheck(string s1,string s2){
     // String 1 in String 2
     if (s2.find(s1) != std::string::npos) return true;
     else return false;
+}
+
+// vector has to sorted
+void rem_dup_order(vector<ll> &vec1) {
+  auto it = unique(vec1.begin(), vec1.end());
+    vec1.resize(distance(vec1.begin(), it));
 }
 
 // For creation of Boolean Matrix 
