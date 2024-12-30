3d859b1bdba8dfe905d25bd42e768b01653d17fd
Shivanshu Mishra <112168836+Ryomensukuna2003@users.noreply.github.com>
2024-12-30 14:47:32 +0530

    Update PBDS info

--- Implementation.cpp
+++ Implementation.cpp
@@ -1,14 +1,25 @@
 //                     「本 物 の 柔 術 を 見 せ て や る」
 
 #include <bits/stdc++.h>
-using namespace  std;
+using namespace std;
 
 // --------------------------------PBDS--------------------------------------------------
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace  __gnu_pbds;
-typedef tree<ll, null_type, less<ll>, rb_tree_tag,tree_order_statistics_node_update> pbds;
-// --------------------------------PBDS--------------------------------------------------
+using namespace __gnu_pbds;
+
+// PBDS (Policy-Based Data Structure) with long long type
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+// PBDS Functions:
+// find_by_order(k): Returns an iterator to the k-th smallest element (0-based index)
+// order_of_key(x): Returns the number of elements in the set less than x
+// Example:
+// pbds s;
+// s.insert(10); s.insert(20); s.insert(30);
+// cout << *s.find_by_order(1); // Output: 20
+// cout << s.order_of_key(25);  // Output: 2
+// --------------------------------------------------------------------------------------
 
 // --------------------------------Mod--------------------------------------------------
 ll mod(ll x) { return (x % M); }
@@ -36,7 +47,7 @@ ll mod_div(ll a, ll b){
 
 
 // For checking String 1 in string 2
-bool substringcheck(string s1,string s2){
+bool substringcheck(string s1, string s2){
     // String 1 in String 2
     if (s2.find(s1) != std::string::npos) return true;
     else return false;
@@ -49,38 +60,36 @@ void rem_dup_order(vector<ll> &vec1) {
 }
 
 // For creation of Boolean Matrix 
-bool g[100][100]={};
+bool g[100][100] = {};
 
 // Cool way to check even odd (Faster)
-if(n&1) cout<<"ODD"<<endl;
-else cout<<"EVEN"<<endl;
-    
+if (n & 1) cout << "ODD" << endl;
+else cout << "EVEN" << endl;
 
 // Check if Power of 2 O(1)
-// Not works if n==0
-if(n&(n-1)) cout<<"Not a power of 2"<<endl;
-else cout<<"Power of 2"<<endl;
+// Not works if n == 0
+if (n & (n - 1)) cout << "Not a power of 2" << endl;
+else cout << "Power of 2" << endl;
 
 // If x/(2^k)
-(x>>k)
+(x >> k)
 // If x*(2^k) 
-(x<<k)
+(x << k)
 
- // To find number of set bits in binary
- __builtin_popcount(x); // for int
- __builtin_popcountll(x); // for long long
+// To find number of set bits in binary
+__builtin_popcount(x);  // for int
+__builtin_popcountll(x); // for long long
 
 // Lower bound Index
-cout<<lower_bound(c,c+n,b[j])-c+1<<endl;
+cout << lower_bound(c, c + n, b[j]) - c + 1 << endl;
 // Lower bound on Vector
 vector<ll>::iterator low;
-	low=lower_bound(all(vec1),target);
-	cout<<low-vec1.begin();
-	
+low = lower_bound(all(vec1), target);
+cout << low - vec1.begin();
 
 // Binary Search for lower bound (not exactly lower_bound)
 int binary_search(vector<ll>& nums, int target) {
-  int l = 0,r = nums.size() - 1;
+  int l = 0, r = nums.size() - 1;
   while (l <= r) {
     int mid = l + (r - l) / 2;
     if (nums[mid] == target) {
