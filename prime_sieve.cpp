1904fe477119aaa2228af488379ec0949589bb77
Shivanshu Mishra <112168836+Ryomensukuna2003@users.noreply.github.com>
2025-01-03 09:44:26 +0530

    Update prime_sieve.cpp

--- prime_sieve.cpp
+++ prime_sieve.cpp
@@ -1,11 +1,12 @@
-// Sieve of Eratosthenes 
-vector<bool> sieve(ll n){
+// Enhanced Sieve of Eratosthenes
+vector<bool> sieve(ll n) {
     vector<bool> prime(n + 1, true);
     prime[0] = prime[1] = false;
-    for (ll p = 2; p * p <= n; p++){
-        if (prime[p]){
-            for (ll i = p * p; i <= n; i += p)
+    for (ll p = 2; p * p <= n; p++) {
+        if (prime[p]) {
+            for (ll i = p * p; i <= n; i += p) {
                 prime[i] = false;
+            }
         }
     }
     return prime;
